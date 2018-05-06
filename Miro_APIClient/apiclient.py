bffa266f4e692b95ca71146c15ce51c84b750539
msonalidhru <38998950+msonalidhru@users.noreply.github.com>
2018-05-06 11:09:29 -0700

    Update apiclient.py

--- Miro_APIClient/apiclient.py
+++ Miro_APIClient/apiclient.py
@@ -1,10 +1,39 @@
 import requests
 
-apiUrl = 'http://127.0.0.1:8000/Api/?studyid=json&subjectID=a'
+apiUrl = 'http://mailtosonalid.pythonanywhere.com/api/?studyid=json&subjectID=a'
 apiUName = 'apiuser'
 apiPwd = 'mirouser'
 
+print("-------------------------")
+print("---- Valid Request  -----")
+print("-------------------------")
 resp = requests.get(apiUrl, auth=(apiUName, apiPwd))
-print("---- API Status Code: -----" + str(resp.status_code) +"\n")
-print("---- API Returned Value -----")
+print("Status Code: " + str(resp.status_code) +"\n")
+print("API Returned Value ")
+print(resp.text)
+
+print("------------------------------------------------------------")
+print("---- Invalid Request: User Authorization not provided  -----")
+print("------------------------------------------------------------")
+resp = requests.get(apiUrl)
+print("Status Code: " + str(resp.status_code) +"\n")
+print("API Returned Value ")
+print(resp.text)
+
+print("------------------------------------------------------------")
+print("---- Invalid Request: studyid not provided  ----------------")
+print("------------------------------------------------------------")
+apiUrl = 'http://mailtosonalid.pythonanywhere.com/api/?subjectID=a'
+resp = requests.get(apiUrl, auth=(apiUName, apiPwd))
+print("Status Code: " + str(resp.status_code) +"\n")
+print("API Returned Value ")
+print(resp.text)
+
+print("------------------------------------------------------------")
+print("---- Invalid Request: subjectID not provided  ----------------")
+print("------------------------------------------------------------")
+apiUrl = 'http://mailtosonalid.pythonanywhere.com/api/?studyid=a'
+resp = requests.get(apiUrl, auth=(apiUName, apiPwd))
+print("Status Code: " + str(resp.status_code) +"\n")
+print("API Returned Value ")
 print(resp.text)
